3da9cc72049544a67b4193ac94c0534ca5a5f578
LegoStax <legostax@gmail.com>
2016-12-24 09:43:57 -0700

    Ready for release

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # md-newtab
 Material design new tab page for Chromium
+
+## Features
+- Quick links to your most used websites
+
+- Google search bar with animations
+
+- Bookmarks widget
+
+## TODO
+- Make quick links customizable on options page
+
+- Allow Google search to be changed to a different default
--- bkmk.js
+++ bkmk.js
@@ -3,22 +3,22 @@ var appendIDs = [];
 appendIDs.push(mkcont);
 function processBookmarks(bookmarks){
     for(var i = 0; i < bookmarks.length; i++) (function(i){
-        console.log(bookmarks[i].id+" - "+bookmarks[i].title+": "+bookmarks[i].url);
+        //console.log(bookmarks[i].id+" - "+bookmarks[i].title+": "+bookmarks[i].url);
         if(!bookmarks[i].url){ // folder
             if(bookmarks[i].id !== "0"){
                 $(mkcont).append('<a id="'+bookmarks[i].id+'-bkmk" class="bkmk-entry" title="'+bookmarks[i].title+'" data-ripple href="#"><i class="material-icons">folder</i>&nbsp;'+bookmarks[i].title+'</a><div id="'+bookmarks[i].id+'-sub" class="bkmk-subcont">');
                 $("#"+bookmarks[i].id+"-bkmk").click(function(){
                     $("#"+bookmarks[i].id+"-sub").slideToggle();
-                    console.log("slide toggle: #"+bookmarks[i].id+"-bkmk");
+                    //console.log("slide toggle: #"+bookmarks[i].id+"-bkmk");
                 });
                 if(bookmarks[i].children){
-                    console.log("start folder contents");
+                    //console.log("start folder contents");
                     appendIDs.push("#"+bookmarks[i].id+"-sub");
                     processBookmarks(bookmarks[i].children);
                 }
                 $(mkcont).append("</div>");
                 appendIDs.pop();
-                console.log("end folder contents");
+                //console.log("end folder contents");
             }
         }else{ // entry
             if(bookmarks[i].id !== "0")
@@ -26,7 +26,7 @@ function processBookmarks(bookmarks){
         }
         //$(mkcont).append('<a class="bkmk-entry" data-ripple href="'+bookmarks[i].url+'">'+bookmarks[i].title+'</a>');
         if((bookmarks[i].id === "0") && (bookmarks[i].children)){
-            console.log("isChildren");
+            //console.log("isChildren");
             processBookmarks(bookmarks[i].children);
         }
     })(i);
